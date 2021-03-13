6e911fb8e006694495129fafc0c9474d3960ac85
Akkiran, Hüseyin <hueseyin.akkiran@haw-hamburg.de>
2021-03-13 11:10:33 +0100

    current state

--- lib/widgets/SizeSelector.dart
+++ lib/widgets/SizeSelector.dart
@@ -1,29 +1,64 @@
 import 'package:flutter/material.dart';
 
-class SizeSelector extends StatelessWidget {
+class SizeSelector extends StatefulWidget {
   final Function _selectIt;
   final List<String> _sizes;
-  const SizeSelector(this._selectIt, this._sizes);
+  SizeSelector(this._selectIt, this._sizes);
 
   @override
+  _SizeSelectorState createState() => _SizeSelectorState();
+}
+
+class _SizeSelectorState extends State<SizeSelector> {
+  Offset tapOffset;
+  @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.max,
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: this
-          ._sizes
-          .map((size) => GestureDetector(
-                onTap: () {
-                  this._selectIt(size);
-                },
-                child: Text(
-                  size,
-                  style: TextStyle(
-                    fontSize: 20,
-                  ),
-                ),
-              ))
-          .toList(),
+    Map<String, GlobalKey> gKeysMap = new Map();
+    for (String size in this.widget._sizes) {
+      gKeysMap[size] = new GlobalKey();
+    }
+    return Stack(
+      children: [
+        if (tapOffset != null)
+          AnimatedPositioned.fromRect(
+            rect: Rect.fromCircle(
+              center: tapOffset,
+              radius: 60.0,
+            ),
+            child: Container(width: 80, height: 80, color: Colors.red),
+            duration: Duration(milliseconds: 400),
+            curve: Curves.bounceOut,
+          ),
+        Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: this
+              .widget
+              ._sizes
+              .map((size) => GestureDetector(
+                    onTapDown: (details) {
+                      this.widget._selectIt(size);
+                      RenderBox rBox =
+                          gKeysMap[size].currentContext.findRenderObject();
+                      print(gKeysMap[size].currentContext.size.width);
+                      // print(rBox.localToGlobal(Offset.zero).dx);
+                      setState(() {
+                        tapOffset = Offset(
+                            gKeysMap[size].currentContext.size.width, 20);
+                        print("offset: ${tapOffset}");
+                      });
+                    },
+                    child: Text(
+                      size,
+                      key: gKeysMap[size],
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ))
+              .toList(),
+        ),
+      ],
     );
   }
 }
